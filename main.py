9173c4d95af267ef6c810be54b93ebfd8e462a0f
david <david.nicola.luu@gmail.com>
2023-05-11 11:24:28 +0300

    Exercise 1 : Sum of 2 numbers input

--- main.py
+++ main.py
@@ -8,9 +8,19 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hei, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
+def exo1():
+    print("Hello World")
+    a = input("Type the first number : ")
+    b = input("Type the second number : ")
+
+    print(f'The value of a is : {a}')
+    print(f'The value of b is : {b}')
+    print(f'The sum of the 2 numbers is  : {a + b}')
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('David')
+    #print_hi('David')
+    exo1()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
